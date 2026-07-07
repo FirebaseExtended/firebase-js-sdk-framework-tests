8315b787fa2bacd724b7c53765b8264dd3211fec
DellaBitta <drsanta@google.com>
2026-07-07 18:25:57 -0400

    added callTest

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,9 @@
-
 // Force source code update
-const { onRequest } = require("firebase-functions/v2/https");
+const { onRequest, onCall } = require("firebase-functions/v2/https");
 
 const logger = require("firebase-functions/logger");
 
-exports.helloWorld = onRequest((request, response) => {
-  response.send("hellooo");
+exports.callTest = onCall((request) => {
+  // request.data will contain { data: 'blah' } from the client
+  return { word: "hellooo" };
 });
-
